a7e8a52e36a45ae6a161174f47f2414b3abb43d7
ESaracay <evansito14@gmail.com>
2023-11-07 08:56:56 -0800

    Updated videos

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -8,7 +8,8 @@ export const Services = (props) => {
           <h2>Live Demo</h2>
         </div>
         <div className="row">
-          <iframe width="800px" height="600px" src="https://www.youtube.com/embed/08HemdY4nf4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="800px" height="600px" src="https://www.youtube.com/embed/6kP3--VBP2M?si=AEK1OEcjxqQ7uDga" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe width="800px" height="600px" src="https://www.youtube.com/embed/6otTM5kZHC8?si=OsgWejG18alovPZX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       </div>
     </div>
